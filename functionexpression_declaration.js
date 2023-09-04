8bcc21f4172503dd80cd85574dc45325bb932937
Dhaval <joshidhaval2002@gmail.com>
2023-09-04 20:00:46 +0530

    Function Expression in JS

--- functionexpression_declaration.js
+++ functionexpression_declaration.js
@@ -1,7 +1,17 @@
 //Function Declaration
 
 function sum(a,b){
-    return a+b
+    return a+b;
 }
 
 console.log(sum(5,5))
+
+
+
+//Function Expression
+
+var sum=function(a,b){        // function without a name is called as "Anonymous Function"
+    return a+b;
+}
+console.log(sum(5,5))
+
