2a7316f10cb9af96df48685b6656912c09886bda
Dhaval <joshidhaval2002@gmail.com>
2023-09-04 20:06:47 +0530

    hoisting of function expression and declaration

--- functionexpression_declaration.js
+++ functionexpression_declaration.js
@@ -1,5 +1,5 @@
 //Function Declaration
-
+//Function declaration  are hoisted and can be called before declaration
 function sum(a,b){
     return a+b;
 }
@@ -10,8 +10,18 @@ console.log(sum(5,5))
 
 //Function Expression
 
-var sum=function(a,b){        // function without a name is called as "Anonymous Function"
+var sum2=function(a,b){        // function without a name is called as "Anonymous Function"
     return a+b;
 }
-console.log(sum(5,5))
+console.log(sum2(5,5))
 
+
+// function expression are hoisted but cannot be called before declaration.
+
+//Example below
+
+/* console.log(sum1(5,5))      //will give error
+   var sum1=function(a,b){       
+    return a+b;
+   } 
+*/
