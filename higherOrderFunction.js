16d53716b94d295ed7d8a757761d15adce55a976
Dhaval <joshidhaval2002@gmail.com>
2023-09-06 20:19:28 +0530

    Converting the code  to higher order function

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -11,29 +11,50 @@
 
 const inputs  =[2,4,6,8,10,12,14,16,18]
 
-function square(input){
-    const squareValues=[];
-    for (let num of input){
+// function square(input){
+//     const squareValues=[];
+//     for (let num of input){
         
-        squareValues.push(num * num);
+//         squareValues.push(num * num);
         
-    }
-    return squareValues
-}
-console.log("Square of all values inside array are as follows:")
-console.log(square(inputs))
+//     }
+//     return squareValues
+// }
+// console.log("Square of all values inside array are as follows:")
+// console.log(square(inputs))
 
 
-function cubic(input){
-    const cubicValues=[];
-    for(let num of input){
-        cubicValues.push(num*num*num)
-    }
-    return cubicValues
-}
-console.log("Cube of all values inside array are as follows:")
-console.log(cubic(inputs))
+// function cubic(input){
+//     const cubicValues=[];
+//     for(let num of input){
+//         cubicValues.push(num*num*num)
+//     }
+//     return cubicValues
+// }
+// console.log("Cube of all values inside array are as follows:")
+// console.log(cubic(inputs))
 
 /*Note in both the function all the other codes are same only the main operation i.e for square and cube 
 is changing and because of that the other lines of code are repeating in coding you should follow
 DRY (Do Not Repeat Yourself) so to follow this we will convert the above function the ocde in Higher Order Function          */
+
+// CREATING A Higher Order Function
+
+function operation(input,fn){
+    output=[]
+    for(let num of input){
+        output.push(fn(num));    //here the fn(num) will be converted to square(num) and the square function is called.
+    }
+    return output;
+}
+
+function square(num){
+    return num*num
+}
+
+function cubic(num){
+    return num*num*num
+}
+
+console.log(operation(inputs,square))
+console.log(operation(inputs,cubic))
